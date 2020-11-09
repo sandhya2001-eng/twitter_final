82e806c361795d0dbd69dcd0aa04b14ed8bcd90b
sandhya2001-eng <73551088+sandhya2001-eng@users.noreply.github.com>
2020-11-09 16:26:51 +0530

    Update mainbot.py

--- mainbot.py
+++ mainbot.py
@@ -35,6 +35,8 @@ while True:
     print(f'Updating Twitter Name : sandhya|{follower_count(user)} Followers')
     print('Waiting to refresh')
     time.sleep(60)
+    
+    
   
                                                                                                              
                    
